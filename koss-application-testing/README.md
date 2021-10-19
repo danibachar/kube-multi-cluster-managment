c4aebcbd60938cf2f288df2c26028b826eabf23f
I <danibachar89@gmail.com>
2021-10-19 12:25:43 -0600

    koss application testing

---
+++ koss-application-testing/README.md
@@ -0,0 +1,90 @@
+
+# Brief explanation on a quick setup of the testing environment
+
+1) Create a GCP Account
+
+2) Open GCP Console
+
+3) Clone the Kubernetes Repo
+
+l
+
+4) Create a Kubernetes Cluster + Autoscale:
+It is important to set the initail cluster size to be enough to test only the HPA mechanism
+
+gcloud container clusters create "microsvc-us" \
+--zone us-central1-a \
+--machine-type "n1-standard-1" \
+--num-nodes 3 --enable-autoscaling --min-nodes 3 --max-nodes 20 \
+--metadata disable-legacy-endpoints=true
+
+5) Build Image
+
+5.1) first we need to get the credentials for the cluster
+gcloud container clusters get-credentials microsvc-us --zone=us-central1-a
+
+5.2) Second we need to build and push the container (Described in the Docker File)
+# Make sure you update the Makefile with your own GCP Project
+
+cd hpa
+make build
+
+6) Deploy Container
+
+# If image is built once and had no changes in code/docker file no need to build, we can just deploy
+# Make sure the image you build is the image described in the .yaml file ( in this specific case hpa.yaml)
+cd hpa
+make deploy
+
+7) monitoring that all is ok
+
+watch kubectl get pods,nodes,hpa,services
+
+# Brief explanation on the kubestl output
+7.1) pods - will show the application pods
+7.2) nodes - will show the cluster nodes
+7.3) hpa - will show the state of the Kubernetes Horizontal Pod Autoscale
+7.4) services - will show the defferent services we created
+7.4.1) LoadBalalcer - the external IP address to access
+7.4.2) Cluster IP
+
+
+# Supplied Applications:
+
+# Create dependencies and pass params 
+- use env var for the containers
+- example that creates config for two dependencies and the load to create on them provided here:
+```
+{
+  "destinations" : [
+    {
+      "target": "http://details.default.svc.cluster.local",
+      "request_payload_kb_size": 50,
+      "config": {
+        "memory_params": {
+          "duration_seconds": 0.2,
+          "kb_count": 50
+        },
+        "cpu_params": {
+          "duration_seconds": 0.2,
+          "load": 0.2
+        }
+      }
+    },
+    {
+      "target": "http://reviews.default.svc.cluster.local",
+      "request_payload_kb_size": 50,
+    "config": {
+        "memory_params": {
+          "duration_seconds": 0.2,
+          "kb_count": 50
+        },
+        "cpu_params": {
+          "duration_seconds": 0.2,
+          "load": 0.2
+        }
+      }
+    }
+  ]
+}
+```
---
+++ koss-application-testing/app.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+from flask.json import loads
+from requests.api import delete
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+import os
+import sys
+import requests
+import asyncio
+import json
+import aiohttp
+from cpu_load_generator import load_single_core
+# Hack to alter sys path, so we will run from microservices package
+# This hack will require us to import with absolut path from everywhere in this module
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(APP_ROOT))
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+def memory_chunk(size_in_kb):
+    l = []
+    for i in range(0, load):
+        l.append("*" * 1024)  # 1KB
+    return l
+
+
+async def generate_memory_load(params):
+    duration_seconds = params.get("duration_seconds", 0.1)  # def 100ms
+    load = params.get("kb_count", 64)  # def 64KB
+    l = memory_chunk(load)
+    asyncio.sleep(duration_seconds)
+    del l
+
+
+async def generate_cpu_load(params):
+    duration_seconds = params.get("duration_seconds", 0.1)  # def 100ms
+    cpu_load = params.get("load", 0.1)  # def 10%
+    core_num = params.get("core_num", 0)  # def 0
+    load_single_core(core_num=core_num,
+                     duration_s=duration_seconds,
+                     target_load=cpu_load)
+    asyncio.sleep(duration_seconds)
+
+
+async def propogate_request():
+    dsts = os.environ.get("DESTINATIONS", "")
+    if dsts == "":
+        return
+    futures = []
+    loop = asyncio.get_event_loop()
+    session = aiohttp.ClientSession()
+    for dst in json.loads(dsts).get("destinations", []):
+        target = dst.get("target", None)
+        if target is None:
+            continue
+        config = dst.get("config", {})
+        payload_kb_size = dst.get("request_payload_kb_size", 10)  # Def 50KB
+        config["dummy_paload_just_for_size"] = memory_chunk(payload_kb_size)
+        f = loop.run_in_executor(None, requests.post, target, None, config)
+        futures.append(f)
+    await asyncio.gather(futures)
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'OK'
+
+
+@app.route('/load/', methods=['POST'])
+async def load():
+    load_options = request.json
+    await asyncio.gather(
+        generate_memory_load(load_options.get('memory_params', {})),
+        generate_cpu_load(load_options.get('cpu_params', {})),
+        propogate_request(),
+    )
+
+    return 'OK'
+
+
+if __name__ == '__main__':
+    # threaded=True is a debugging feature, use WSGI for production!
+    app.run(host='0.0.0.0', port='8081', threaded=True)
